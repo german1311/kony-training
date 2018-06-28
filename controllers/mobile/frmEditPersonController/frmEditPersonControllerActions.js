4bda0080d88b6fd42f70df26bd1c42c2f9ea1b22
German <german1311@outlook.com>
2018-06-28 18:04:55 -0500

    UI fixes

--- controllers/mobile/frmEditPersonController/frmEditPersonControllerActions.js
+++ controllers/mobile/frmEditPersonController/frmEditPersonControllerActions.js
@@ -2,4 +2,14 @@ define({
     /*
       This is an auto generated file and any modifications to it may result in corruption of the action sequence.
     */
+    /** onClick defined for flxHeaderLeft **/
+    AS_FlexContainer_f42fe035fe8a4e14a0d16dc6fe806d32: function AS_FlexContainer_f42fe035fe8a4e14a0d16dc6fe806d32(eventobject) {
+        var self = this;
+        return self.onCancel.call(this);
+    },
+    /** onClick defined for flxHeaderRight **/
+    AS_FlexContainer_e23834281ea8442081cd59a018d701c9: function AS_FlexContainer_e23834281ea8442081cd59a018d701c9(eventobject) {
+        var self = this;
+        return self.onSave.call(this);
+    }
 });

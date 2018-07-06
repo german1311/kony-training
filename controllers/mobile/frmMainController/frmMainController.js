46d3adb209c7e021e3ffc029c72e6ec1872d3c1e
German <german1311@outlook.com>
2018-07-06 18:24:12 -0500

    delete working

--- controllers/mobile/frmMainController/frmMainController.js
+++ controllers/mobile/frmMainController/frmMainController.js
@@ -1,21 +1,26 @@
 /* jshint esnext: true */
 define({
-
     //Type your controller code here 
-
     populateSegment: function(rowData) {
         var dataFormatted = [];
         for (var i = 0; i < rowData.length; i++) {
             const element = rowData[i];
             dataFormatted[i] = {
-                imgProfile: "option3.png",
+                flxLeft: {
+                    onClick: this.onDeleteRowSegmentClick
+                },
+                flxRight: {
+                    onClick: this.onRowSegmentClick
+                },
+                imgProfile: {
+                    src: "option3.png",
+                },
                 lblFullName: element.FirstName + " " + element.LastName,
                 lblStatus: element.Active ? "" : "Off",
                 imgDelete: "icons8_minus_48.png",
                 primaryKey: element.Id
             };
         }
-
         this.view.segPersons.removeAll();
         this.view.segPersons.addAll(dataFormatted);
     },
@@ -24,12 +29,11 @@ define({
         editForm.navigate();
     },
     onDeleteClick: function() {
-        this.animateSegmentOnDelete(this.isDeleting);
         this.isDeleting = !this.isDeleting;
+        this.animateSegmentOnDelete(this.isDeleting);
     },
     onPersonsClick: function() {
         var self = this;
-
         personModel.get().then((data) => {
             self.populateSegment(data);
             self.view.mainContainer.setActiveFooterMenu(1);
@@ -48,57 +52,62 @@ define({
             this.view.search.disableFullInput();
             return;
         }
-
         personModel.findPerson(e.text).then((data) => {
             self.populateSegment(data);
         }).catch(Util.logError);
     },
     onDeleteRowSegmentClick: function(args) {
-        console.log(args);
+        let person = this.view.segPersons.selectedRowItems[0];
+        let self = this;
+        let basicProperties = {
+            message: `Seguro que quiere eliminar a: ${person.lblFullName}?`,
+            alertType: constants.ALERT_TYPE_CONFIRMATION,
+            yesLabel: "Si",
+            alertHandler: (params) => {
+                console.log(params);
+                personModel.delete(person.primaryKey).then(self.onPersonsClick).catch(Util.logError);
+            }
+        };
+
+        let platformSpecificProperties = {};
+        kony.ui.Alert(basicProperties, platformSpecificProperties);
+
+
     },
     onRowSegmentClick: function(eventObject, rowNumber) {
         var editForm = new kony.mvc.Navigation("frmEditPerson");
-
-        editForm.navigate(eventObject.selectedRowItems[0]);
+        editForm.navigate(this.view.segPersons.selectedRowItems[0]);
     },
     onInit: function() {
         let PersonModelClass = require("personModel");
         var self = this;
         self.isDeleting = false;
         setupSync().then(() => {
-                if (!personObjectService) {
-                    personObjectService = dataBase.performObjectService(PersonServiceConfig.name, {
-                        "access": "offline"
-                    });
-                }
-
-                if (!personModel) {
-                    personModel = new PersonModelClass(new kony.sdk.KNYObj(PersonServiceConfig.objects.person.name));
-                }
-
-                personModel.startSync()
-                    .then(self.onPersonsClick);
-            })
-            .catch(Util.logError);
+            if (!personObjectService) {
+                personObjectService = dataBase.performObjectService(PersonServiceConfig.name, {
+                    "access": "offline"
+                });
+            }
+            if (!personModel) {
+                personModel = new PersonModelClass(new kony.sdk.KNYObj(PersonServiceConfig.objects.person.name));
+            }
+            personModel.startSync().then(self.onPersonsClick);
+        }).catch(Util.logError);
         console.log("onInit called");
     },
     animateSegmentOnDelete: function(isDeleting) {
         let finalWidthLeft = "0%";
         let finalWidthRight = "100%";
         this.view.lblLeft.text = "Delete"
-
         if (isDeleting) {
-            finalWidthLeft = "20%";
-            finalWidthRight = "80%";
+            finalWidthLeft = "15%";
+            finalWidthRight = "85%";
             this.view.lblLeft.text = "Done"
         }
-
         let transformPropOne = kony.ui.makeAffineTransform();
         transformPropOne.scale(1, 0);
-
         let transformPropTwo = kony.ui.makeAffineTransform();
         transformPropTwo.scale(1, 1);
-
         let animationDefinitionLeft = {
             0: {
                 "transform": transformPropOne,
@@ -108,7 +117,6 @@ define({
                 "width": finalWidthLeft
             }
         };
-
         let animationLeft = kony.ui.createAnimation(animationDefinitionLeft);
         let animationConfig = {
             "duration": 0.3,
@@ -116,33 +124,25 @@ define({
             "delay": 0,
             "fillMode": kony.anim.FILL_MODE_FORWARDS
         };
-
         let animationCallbacksLeft = {
             animationStart: (flexContainer) => {
                 flexContainer.width = finalWidthLeft;
-                Util.logError("animation segment row has begun");
+                // Util.logError("animation segment row has begun");
             },
             animationEnd: (flexContainer) => {
-                // this.view.segPersons.flxFul
-                Util.logError("animation flex ends");
+                // Util.logError("animation flex ends");
             }
         }
-
         let fullAnimationLeft = {
             definition: animationLeft,
             config: animationConfig,
             callbacks: animationCallbacksLeft
         };
-
         this.view.segPersons.animateRows({
-            rows: [{
-                rowIndex: 0,
-                sectionIndex: 0
-            }],
+            rows: this.getSegmentRows(),
             widgets: ["flxLeft"],
             animation: fullAnimationLeft
         });
-
         let animationDefinitionRight = {
             0: {
                 "transform": transformPropOne,
@@ -152,14 +152,13 @@ define({
                 "width": finalWidthRight
             }
         };
-
         let animationCallbacksRight = {
             animationStart: (flexContainer) => {
                 flexContainer.width = finalWidthRight;
-                Util.logError("animation segment row has begun");
+                // Util.logError("animation segment row has begun");
             },
             animationEnd: (flexContainer) => {
-                Util.logError("animation flex ends");
+                // Util.logError("animation flex ends");
             }
         }
         let animationRight = kony.ui.createAnimation(animationDefinitionRight);
@@ -168,14 +167,21 @@ define({
             config: animationConfig,
             callbacks: animationCallbacksRight
         };
-
         this.view.segPersons.animateRows({
-            rows: [{
-                rowIndex: 0,
-                sectionIndex: 0
-            }],
+            rows: this.getSegmentRows(),
             widgets: ["flxRight"],
             animation: fullAnimationRight
         });
+    },
+    getSegmentRows: function() {
+        var rowList = [];
+        for (var i = 0; i < this.view.segPersons.data.length; i++) {
+            var rowItem = {
+                sectionIndex: 0,
+                rowIndex: i
+            };
+            rowList.push(rowItem);
+        }
+        return rowList;
     }
 });
